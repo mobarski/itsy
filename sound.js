d4caadc6029cd330a88bc618286ada5ff4255b72
Maciej Obarski <mobarski@gmail.com>
2022-06-21 00:26:14 +0200

    audio prototype

--- sound.js
+++ sound.js
@@ -1,2 +1,27 @@
-// TODO: use howler.js
+var context = new AudioContext()
+var osc = context.createOscillator()
+var vol = context.createGain()
+vol.connect(context.destination)
+
+osc.type = 'square'
+//osc.type = 'sine'
+osc.connect(vol)
+function sound(f,t) {
+	//vol.gain.exponentialRampToValueAtTime(1.0, 0.3) // fade in
+	vol.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + t) // fade out
+	osc.frequency.value = f
+	//osc.frequency.setValueAtTime(0, context.currentTime + t*0.3)
+	//osc.frequency.setValueAtTime(f*4, context.currentTime + t*0.8)
+	osc.start(0)
+	osc.stop(context.currentTime + t)
+}
+sound(220, 0.4)
+
+
+// REF: https://bashooka.com/coding/web-audio-javascript-libraries/
+// REF: http://bit101.github.io/tones/
+// REF: https://stackoverflow.com/questions/6343450/generating-sound-on-the-fly-with-javascript-html5
+// REF: https://marcgg.com/blog/2016/11/01/javascript-audio/
+// TODO: use howler.js ???
+
 
