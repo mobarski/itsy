d03ca04d99476d7cf19c3187f309aa68cf85f08a
Maciej Obarski <mobarski@gmail.com>
2022-06-23 08:22:57 +0200

    mouse refactoring

--- js/itsy.js
+++ js/itsy.js
@@ -145,11 +145,12 @@ function fullscreen() {
 
 // ===[ mouse.js ]=================
 
-// TODO: mouse - x,y,button1
-// TODO: left,right,up,down A B
-// TODO: start / select
 
 fc.has_mouse = true
+
+function mouse() {
+	// TODO
+}
 
 function set_mouse_xy(e) {
 	let bcr = fc.cnv.getBoundingClientRect()
