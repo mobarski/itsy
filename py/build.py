ff39fe3b9867f0196bd395aa1ebe46cc86448252
Maciej Obarski <mobarski@gmail.com>
2022-07-05 08:23:20 +0200

    option to minify js code

--- py/build.py
+++ py/build.py
@@ -1,3 +1,5 @@
+MINIFY = True
+
 PATH = '../js/itsy.js'
 
 FILES = [
@@ -6,14 +8,21 @@ FILES = [
 	"../js/screen.js",
 	"../js/mouse.js",
 	#"../js/experimental.js",
-	#"../js/sound.js",
+	"../js/sound.js",
 	"../js/font.js",
 	"../js/rom.js",
 ]
+
+if MINIFY:
+	import jsmin
+	minify = jsmin.jsmin
 
 with open(PATH, 'w') as fo:
 	for filepath in FILES:
 		filename = filepath.split('/')[-1] # TODO
 		raw = open(filepath).read()
-		print(f'// ===[ {filename} ]=================\n', file=fo)
+		if MINIFY:
+			raw = minify(raw, quote_chars="'\"`")
+		else:
+			print(f'// ===[ {filename} ]=================\n', file=fo)
 		print(raw, file=fo)
